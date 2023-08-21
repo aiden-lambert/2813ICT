a024a600e9e7de20f19e032a1de2fb46a6d94b8c
aiden-lambert <lambert@labrys.io>
2023-08-21 20:59:34 +1000

    changed readme and some eslint

--- PrototypeBase/ForestHealthPlatform/clientapp/.eslintrc.cjs
+++ PrototypeBase/ForestHealthPlatform/clientapp/.eslintrc.cjs
@@ -1,18 +1,25 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true, // Add this line!
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    camelcase: "error",
+    "spaced-comment": "error",
+    quotes: ["error", "single"],
+    "no-duplicate-imports": "error",
+  },
+};
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # 2813ICT
 
-Clone repo to make changes to the TeamReport 
+Clone repo to make changes to the TeamReport
 Will set up the basic project repo shortly
+
+Install the dotnet SDK from Msoft
+Navigate to the project folder and run the following commands
+`dotnet nuget add source "https://api.nuget.org/v3/index.json" --name "NuGet official package source"`
+
+`dotnet restore`
+
+You can then run the app qith `dotnet run`
+
+Nagivate to /app to see the app running
